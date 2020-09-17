7013fc2f9a5a7abee609d317762b0c63735b543b
HugoBinati <HugoBinati@hotmail.com>
2020-09-17 19:41:13 -0300

    atualizacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatExpansionModule} from '@angular/material/expansion';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { DialogComponent, HomeComponent } from './home/home.component';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +32,13 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     ReactiveFormsModule,
     FormsModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import { TaskState } from './../models/task-state';
 import { Task } from './../models/task';
 import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-home',
@@ -13,8 +14,9 @@ export class HomeComponent implements OnInit {
 
   panelOpenState = false;
   taskList: Task[];
+  displayedColumns: string[] = ['tasks', 'actions',];
 
-  constructor(protected _http: HttpClient) { }
+  constructor(protected _http: HttpClient, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.findAll();
@@ -22,20 +24,61 @@ export class HomeComponent implements OnInit {
 
   findAll() {
     this._http.get<Task[]>(environment.apiUrl + '/tasks').subscribe(res => {
-      this.taskList = res;
+      this.taskList = res.sort((a, b) => (a.id < b.id) ? 1 : -1);
     })
   }
 
-  save() {
-    this._http.post<any>(environment.apiUrl + '/tasks', 'teste').subscribe(res => { this.findAll(); });
+  save(task: Task) {
+    this._http.post<any>(environment.apiUrl + '/tasks', task.text).subscribe(res => { this.findAll(); });
   }
 
-  deleteAll(){
+  update(task: Task) {
+    task.taskState = TaskState.DONE;
+    this._http.put(environment.apiUrl + '/tasks/' + task.id, task).subscribe(res => { this.findAll(); });
+  }
+
+  updateTask(task: Task){
+    this._http.put(environment.apiUrl + '/tasks/' + task.id, task).subscribe(res => { this.findAll(); });
+  }
+
+  clear() {
     this._http.delete(environment.apiUrl + '/tasks').subscribe(res => { this.findAll(); });
   }
 
-  update(task: Task){
-    task.taskState=TaskState.DONE;
-    this._http.put(environment.apiUrl + '/tasks/' + task.id, task).subscribe(res => { this.findAll(); });
+  delete(task: Task) {
+    this._http.delete(environment.apiUrl + '/tasks/' + task.id).subscribe(res => { this.findAll(); });
   }
+
+  openDialog(task: Task = new Task()): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '750px',
+      data: task
+    });
+
+    dialogRef.afterClosed().subscribe(text => {
+      if (text !== undefined)
+        this.save(text);
+    });
+  }
+
+}
+
+@Component({
+  selector: 'dialog-component',
+  templateUrl: './dialog.component.html',
+})
+export class DialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public  data: {object: task, action: action}) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  saveTask(t) {
+    this.dialogRef.close(t);
+  }
+
 }
